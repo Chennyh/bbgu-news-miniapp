7303fe8f70f62068d6f5148395eac9c585f43ba6
Chennyh <chennyh@qq.com>
2020-12-18 09:39:20 +0800

    init project

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		onError: function(err) {
+			console.log('App Error:' + err)
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "uview-ui/index.scss";
+	/*每个页面公共css */
+</style>
